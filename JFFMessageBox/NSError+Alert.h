f2a294daa3618458269019de77bf9951292e3cb5
Igor Palaguta <igor.palaguta@gmail.com>
2014-04-14 20:04:09 +0300

    add handler for errors

--- JFFMessageBox/NSError+Alert.h
+++ JFFMessageBox/NSError+Alert.h
@@ -1,6 +1,10 @@
 #import <Foundation/Foundation.h>
 
+typedef BOOL (^JFFAlertViewErrorHandler)( NSString* title_, NSError* error_ );
+
 @interface NSError (Alert)
+
++(void)addErrorHandler:( JFFAlertViewErrorHandler )handler_;
 
 -(void)showAlertWithTitle:( NSString* )title_;
 
--- JFFMessageBox/NSError+Alert.m
+++ JFFMessageBox/NSError+Alert.m
@@ -2,16 +2,31 @@
 
 #import "JFFAlertView.h"
 
+static NSMutableArray* error_handlers_ = nil;
+
 @implementation NSError (Alert)
 
--(void)printToLog
++(void)addErrorHandler:( JFFAlertViewErrorHandler )handler_
 {
-   NSLog( @"NSError : %@, domain : %@ code : %d", [ self localizedDescription ], [ self domain ], [ self code ] );
+   JFFAlertViewErrorHandler handler_copy_ = [ handler_ copy ];
+   if ( !error_handlers_ )
+   {
+      error_handlers_ = [ NSMutableArray arrayWithObject: handler_copy_ ];
+   }
+   else
+   {
+      [ error_handlers_ addObject: handler_copy_ ];
+   }
 }
 
 -(void)showAlertWithTitle:( NSString* )title_
 {
-   [ self printToLog ];
+   for ( JFFAlertViewErrorHandler handler_ in error_handlers_ )
+   {
+      if ( handler_( title_, self ) )
+         return;
+   }
+
    [ JFFAlertView showAlertWithTitle: title_ description: [ self localizedDescription ] ];
 }
 
